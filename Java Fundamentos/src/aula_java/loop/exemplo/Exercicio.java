da80152fa2a25bc2b867ffb4b4cca8e350409e0a
Camila Tribess <31188@PROWAY.TREINA>
2020-10-21 21:50:56 -0300

    novos

--- Java Fundamentos/src/aula_java/loop/exemplo/Exercicio.java
+++ Java Fundamentos/src/aula_java/loop/exemplo/Exercicio.java
@@ -3,29 +3,33 @@ package aula_java.loop.exemplo;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 public class Exercicio {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Escreva a quantidade de alunos presentes na sala");
-		int quantidadeAlunos = Integer.parseInt(sc.nextLine());
-
-		String[] vetorNomeAluno = new String[quantidadeAlunos];
-		
-
+		int quantidadeAlunos = Integer.parseInt(sc.nextLine());//recebe a quantidade de alunos
+		String[] vetorNomeAluno = new String[quantidadeAlunos];//relação de quantidade de alunos e nomes informardos
+		//recebe a quantidade de alunos, saindo do valor zero
 		for(int i = 0; i < quantidadeAlunos; i++) {
-			System.out.println("Escreva o nome do aluno " + (i+1));
-			vetorNomeAluno[i] = sc.nextLine();
+			System.out.println("Escreva o nome do aluno " + (i+1));//pede para informar o nome
+			vetorNomeAluno[i] = sc.nextLine(); //recebe o nome do aluno
 
 		}
-        Arrays.sort(vetorNomeAluno);
+		//esse
+        Arrays.sort(vetorNomeAluno);//ordenando o vetor
+        System.out.println("Imprimindo com for");
 		for(int i = 0; i < quantidadeAlunos; i++) {
 			System.out.println(i+1 + " - " + vetorNomeAluno[i]);
 		}
+		//igual a esse
+		System.out.println("Imprimindo com foreach");
+		int i = 0;
+		for(String nome : vetorNomeAluno) {			
+			System.out.println(i + " - " + nome);
+			i++;
+		}
 
 
-		System.out.println();
 	}
 
 }
